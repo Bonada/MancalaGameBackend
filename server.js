aae8754ba9981058e97b68a7ec549551e35e1d67
bonada <bonada@rpi.edu>
2022-02-24 14:33:10 -0500

    z

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const io = require('socket.io')(process.env.PORT || 4000, {
-    cors: {
-        origin: ["*"],
-    },
-})
+const cors = require('cors');
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+const app = express()
+app.use(cors());
+
+const server = http.createServer(app)
+const io = socketio(server)
 
 io.on('connection', client => {
     console.log(client);
 })
+
+server.listen(process.env.PORT || 4000)
