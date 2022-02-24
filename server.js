8bdffa0407755b59a49d0ecbdad0fb44814ba956
bonada <bonada@rpi.edu>
2022-02-24 14:20:58 -0500

    try 6

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
-const cors = require('cors');
-const express = require('express')
-const http = require('http')
-const socketio = require('socket.io')
-const app = express()
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
-
-const server = http.createServer(app)
-const io = socketio(server)
+const io = require('socket.io')(process.env.PORT || 4000, {
+    cors: {
+        origin: ["*"],
+    },
+})
 
 io.on('connection', client => {
     console.log(client);
 })
-
-server.listen(process.env.PORT || 4000)
