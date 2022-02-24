9828f7b8cd58c9f689c1263cb12ba7a327bc1599
bonada <bonada@rpi.edu>
2022-02-24 14:00:10 -0500

    cors

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const cors = require('cors');
 const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 const app = express()
-app.use(cors);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 const server = http.createServer(app)
 const io = socketio(server)
